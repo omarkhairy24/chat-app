4c748a750a1d0cced443bef8bbda485057bb7a7a
omarkhairy24 <omarkairyno7@gmail.com>
2024-11-17 15:18:22 +0200

    adding update password

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
       'http://localhost:8080',
       'http://127.0.0.1:8080',
       'http://192.168.1.4:8080',
-      // 'https://adaptive-behavior.onrender.com'
+      'https://adaptive-behavior.onrender.com'
 
     ],
     credentials:true
